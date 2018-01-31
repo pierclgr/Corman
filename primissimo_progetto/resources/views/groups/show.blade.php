71e169442039269e3e4cf6f75496c16a84006fc8
d-monaco <35458512+d-monaco@users.noreply.github.com>
2018-02-01 00:21:53 +0100

    Commit di Luigi

---
+++ primissimo_progetto/resources/views/groups/show.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-md-offset-0">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    @if(count($group)>0)
+                        <div name="al_latoSX" style="text-align: left;"><h1>{{ $group[0]->nomeGruppo }}</h1><br>{{ $group[0]->descrizioneGruppo }}</div>
+                        <div name="al_latoDX" style="text-align: right;">ESCI</div>
+                    @endif
+                </div>
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    @if(count($group)>0)
+                        @foreach($group as $g)
+                            {{ $g->nomeGruppo }}<br>{{ $g->descrizioneGruppo }}
+                        @endforeach
+                    @else
+                        <strong>NESSUNA PUBBLICAZIONE PRESENTE IN QUESTO GRUPPO!</strong>
+                    @endif
+                    <br><a href="{{route('groups.rintraccia', [$group[0]->idGroup, $group[0]->id] )}}"><button type="button" class="btn btn-primary">Add a publication in this group</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
