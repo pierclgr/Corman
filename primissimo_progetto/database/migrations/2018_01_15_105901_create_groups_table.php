20356f9fb3d0674ccdc9f1fe018b7b3e9ac41386
d-monaco <35458512+d-monaco@users.noreply.github.com>
2018-01-31 20:00:15 +0100

    Modifiche di Luigi

--- primissimo_progetto/database/migrations/2018_01_15_105901_create_groups_table.php
+++ primissimo_progetto/database/migrations/2018_01_15_105901_create_groups_table.php
@@ -15,8 +15,9 @@ class CreateGroupsTable extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->increments('idGroup');
-            $table->string('titolo');
-            $table->string('descrizione');
+            $table->string('nomeGruppo')->unique()->nullable();
+            $table->string('descrizioneGruppo')->nullable();
+            $table->boolean('tipoVisibilita')->nullable();
             $table->timestamps();
         });
     }
--- primissimo_progetto/resources/views/layouts/app.blade.php
+++ primissimo_progetto/resources/views/layouts/app.blade.php
@@ -35,9 +35,9 @@
                     @guest
                         <!-- Niente -->
                     @else
-                       <form class="navbar-form navbar-left" action="/home/search/" method="get">
+                       <form class="navbar-form navbar-left">
                             <div class="form-group has-feedback">
-                                <input type="text" class="form-control" id="searchBar" name="input" onkeyup="helpSearch()" onfocusout="hideDropdown()" placeholder="Search">
+                                <input type="text" class="form-control" id="searchBar" onkeyup="helpSearch()" onfocusout="hideDropdown()" placeholder="Search">
                                 
                                 <ul class="dropdown-menu" id="searchDropdown" style="display: none; style height: 500 overflow: auto;">
                                         <a href="#">res1<br></a>
@@ -140,7 +140,6 @@
 
         function helpSearch(){
             document.getElementById("searchDropdown").style.display="block";
-            
             var input, filter, dropdown, elems, noResults=true;
             input = document.getElementById("searchBar");
             filter = input.value.toUpperCase();
@@ -161,8 +160,7 @@
             if(noResults==true){
                 document.getElementById("NoResults").style.display="block";
             }
-
-            }
+        }
     </script>
     
 </body>
